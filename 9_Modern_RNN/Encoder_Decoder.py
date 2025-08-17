f37b17befc1a2bcb21fc0cba7194647fcbef5bb7
Isaac Liu <ezioliu@foxmail.com>
2025-08-17 23:52:06 +0800

    完成了编码器的代码

--- 9_Modern_RNN/Encoder_Decoder.py
+++ 9_Modern_RNN/Encoder_Decoder.py
@@ -1,4 +1,12 @@
+from pyexpat import features
 from torch import nn
+import math
+import torch
+import collections
+
+
+
+
 
 class Encoder(nn.Module):
     def __init(self,**kwargs):
@@ -25,3 +33,26 @@ class EncoderDecoder(nn.Module):
         enc_outputs = self.encoder(enc_x,*args)
         dec_state = self.decoder.init_state(enc_outputs,*args)
         return self.decoder(dec_x,dec_state)
+class Seq2SeqEncoder(Encoder):
+    def __init__(self, vocab_size,embed_size, num_hiddens, num_layers, dropout = 0, **kwargs):
+        super(Seq2SeqEncoder, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout = dropout)
+        # Gated Recurrent Unit, GRU，门控循环单元,它和nn.LSTM结构相似
+        # torch.nn.GRU(input_size, hidden_size, num_layers = 1, bias = True,batch_first = False,
+        #              dropout = 0)
+        # batch_first代表是否输出以(batch,seq,features)的形式
+    def forward(self,X, *args):
+        X = self.embedding(X)
+        X = X.permute(1,0,2)
+        output, state = self.rnn(X)
+        # output的形状为(num_steps, batch_size,num_hiddens)
+        # state的形状（num_layers,batch_size,num_hiddens）
+        return output, state
+
+# encoder = Seq2SeqEncoder(vocab_size = 10,embed_size = 8,num_hiddens = 16,num_layers = 2)
+# encoder.eval()
+# x = torch.zeros((4,7),dtype = torch.long)
+# output, state = encoder(x)
+
+
