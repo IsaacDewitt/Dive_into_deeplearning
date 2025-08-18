0a29a53deabec5605a3ef824cf7c89a7c77b8b75
Isaac Liu <ezioliu@foxmail.com>
2025-08-19 00:28:50 +0800

    完成了解码器的编写，和部分sequence_mask的内容，用来屏蔽一些无关项。

--- 9_Modern_RNN/Encoder_Decoder.py
+++ 9_Modern_RNN/Encoder_Decoder.py
@@ -49,6 +49,36 @@ class Seq2SeqEncoder(Encoder):
         # output的形状为(num_steps, batch_size,num_hiddens)
         # state的形状（num_layers,batch_size,num_hiddens）
         return output, state
+class Seq2SeqDecoder(Decoder):
+    def __init__(self,vocab_size,embed_size, num_hiddens, num_layers, dropout = 0, **kwargs):
+        super(Seq2SeqDecoder, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size+num_hiddens, num_hiddens, num_layers, dropout = dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+    def init_state(self,enc_outputs, *args):
+        return enc_outputs[1]
+    def forward(self,X, state):
+        # 说实话感觉作者不是特别懂这一块啊,整个这一章有点太,那啥了
+        # 输出大小为(batch_size, num_steps, embed_size)
+        X = self.embeding(X).permute(1,0,2)
+        # 将tensor的维度重新排列,默认是(0,1,2),这里排列成(1,0,2)
+        context = state[-1].repeat(X.shape[0],1,1)
+        # repeat函数表示state最后一个状态在某个方向复制多少次,这里表示在行方向重复X.shape[0]次,其他方向上不重复
+        X_and_context = torch.cat((X,context),2)
+        output,state = self.rnn(X_and_context,state)
+        output = self.dense(output).permute(1,0,2)
+        # output形状变为（batch_size, numsteps,vocab_size)
+        # state的形状为：（num_layers,batch_size,num_hiddens)
+        return output,state
+
+def sequence_mask(X, valid_len,value = 0):
+    maxlen = X.size(1)
+    mask = torch.arange(maxlen,dtype = torch.float32, device = X.device)[None,:]<valid_len[:,None]
+    # torch.arange(start=0, end, step=1, *, dtype=None, device=None)
+    # 行为类似range，在这里的None的行为是在张量中插入一个新维度，这里防止mask变成(maxlen,)，使其成为(1, maxlen)
+    # 这里利用了广播特性，<左边的大小为[1,maxlen],右边为valid_len[batch_size,1],然后两个都会变成
+    # [batch_size,maxlen],进行比较
+
 
 # encoder = Seq2SeqEncoder(vocab_size = 10,embed_size = 8,num_hiddens = 16,num_layers = 2)
 # encoder.eval()
